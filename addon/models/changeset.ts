3f8854ec08fec1c7b2c2262ea5a40f0fff6c22bd
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-09-11 12:35:18 -0700

    awrite down idea' g

---
+++ addon/models/changeset.ts
@@ -0,0 +1,27 @@
+// Todo: lookup the interface for Yup validators.
+export class ChangeSet<PersistenceModel> {
+  public data;
+  private validator;
+
+  constructor(persistenceModel: PersistenceModel, validator) {
+    this.data = this.load(persistenceModel);
+    this.validator = validator;
+  }
+
+  // what situations would we need to re-load the data?
+  //  - if there is data in local storage, maybe this has
+  //    some key we can use to pull things out of local storage
+  //    and return a populated data object.
+  load(persistenceModel: PersistenceModel) {
+    return persistenceModel;
+  }
+
+  /**
+   *  Will only write if the validator succeeds
+   */
+  write() {
+
+  }
+
+
+}
