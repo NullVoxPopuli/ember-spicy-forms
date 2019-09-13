c64bf93f965484e89bc36658e24eb362a4b02d09
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-09-13 07:49:38 -0700

    update signature

--- addon/components/spicy-form.ts
+++ addon/components/spicy-form.ts
@@ -1,18 +1,38 @@
 import Component from '@glimmer/component';
 import layout from '../templates/components/spicy-form';
 
+type Task<Args, Return> = unknown; // e-concurrency Task tbd;
+type ChangeSet = unknown; // details tbd;
+type OnSubmitFn = (changeSet: ChangeSet) => Promise<void>;
+type OnSubmitTask = Task<[ChangeSet], void>;
+
 type Args =
   {
     form?: unknown;
     validator?: unknown;
-    onSubmit?: () => Promise<void>;
+    onSubmit?: OnSubmitTask | OnSubmitFn;
   }
   |
   {
-    changeset?: unknown;
+    changeset?: ChangeSet;
+    onSubmit?: OnSubmitTask | OnSubmitFn;
   }
 ;
 
 export default class SpicyForm extends Component<Args> {
   layout = layout
+
+  get changeset() {
+    // not sure yet what should be here, or if it even needs
+    // to be different from the passed in changeset
+    return;
+  }
+
+  /*
+   * @(task(function*(){
+   *   yield this.args.onSubmit(this.changeset);
+   * }).drop());
+   * onSubmit
+   *
+   */
 }
